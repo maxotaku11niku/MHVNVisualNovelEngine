5d94e6451867957618d3384e42124fc856918794
maxotaku11niku <maxotaku3901@gmail.com>
2023-11-18 02:31:10 +0000

    MHVN98: Test whole program optimisation
    Do a test of what a whole-program optimisation would do, fixing any errors that came up.

--- MHVN98/src/doscalls.h
+++ MHVN98/src/doscalls.h
@@ -48,15 +48,15 @@
 #define FILE_OPEN_WRITE 0x01
 #define FILE_OPEN_READWRITE 0x02
 //INT 21 function 3E - Close File (given by handle in 'handle', can return an error code)
-#define int21_closefile(handle, errorcode, errorflag) asm inline ("movb $62, %%ah\n\tint $33" : "=a" (errorcode), "=@ccc" (errorflag) : "b" (handle))
+#define int21_closefile(handle, errorcode, errorflag) asm volatile ("movb $62, %%ah\n\tint $33" : "=a" (errorcode), "=@ccc" (errorflag) : "b" (handle))
 //INT 21 function 3F - Read From a File (given by handle in 'handle', puts 'len' bytes into array pointed to by 'buffer', returns the number of bytes actually read in 'readbytes', can return an error code)
-#define int21_readfile(handle, len, bufseg, buffer, readbytes, errorflag) asm inline ("movw %w2, %%ds\n\tmovb $63, %%ah\n\tint $33\n\tmovw %%cs, %w2\n\tmovw %w2, %%ds" : "=a" (readbytes), "=@ccc" (errorflag), "+r" (bufseg) : "b" (handle), "c" (len), "d" (buffer))
+#define int21_readfile(handle, len, bufseg, buffer, readbytes, errorflag) asm inline ("movw %w2, %%ds\n\tmovb $63, %%ah\n\tint $33\n\tmovw %%cs, %w2\n\tmovw %w2, %%ds" : "=a" (readbytes), "=@ccc" (errorflag), "+r" (bufseg), "=m" (*((unsigned char*)buffer)) : "b" (handle), "c" (len), "d" (buffer))
 //INT 21 function 40 - Write To a File (given by handle in 'handle', puts 'len' bytes into the file from the array pointed to by 'buffer', returns the number of bytes actually written in 'writebytes', can return an error code)
-#define int21_writefile(handle, len, bufseg, buffer, writebytes, errorflag) asm inline ("movw %w2, %%ds\n\tmovb $64, %%ah\n\tint $33\n\tmovw %%cs, %w2\n\tmovw %w2, %%ds" : "=a" (writebytes), "=@ccc" (errorflag), "+r" (bufseg) : "b" (handle), "c" (len), "d" (buffer))
+#define int21_writefile(handle, len, bufseg, buffer, writebytes, errorflag) asm inline ("movw %w2, %%ds\n\tmovb $64, %%ah\n\tint $33\n\tmovw %%cs, %w2\n\tmovw %w2, %%ds" : "=a" (writebytes), "=@ccc" (errorflag), "+r" (bufseg) : "b" (handle), "c" (len), "d" (buffer), "m" (*((unsigned char*)buffer)))
 //INT 21 function 41 - Delete File (pathname pointed to by 'path' (null-terminated), can return an error code)
-#define int21_deletefile(pathseg, path, errorcode, errorflag) asm inline ("movw %w2, %%ds\n\tmovb $65, %%ah\n\tint $33\n\tmovw %%cs, %w2\n\tmovw %w2, %%ds" : "=a" (errorcode), "=@ccc" (errorflag), "+r" (pathseg) : "d" (path))
+#define int21_deletefile(pathseg, path, errorcode, errorflag) asm volatile ("movw %w2, %%ds\n\tmovb $65, %%ah\n\tint $33\n\tmovw %%cs, %w2\n\tmovw %w2, %%ds" : "=a" (errorcode), "=@ccc" (errorflag), "+r" (pathseg) : "d" (path))
 //INT 21 function 42 - Seek In File (given by handle in 'handle', moves the file pointer by chunklen:len bytes, according to the method in 'method', returns the new file position in poschunk:pos, can return an error code)
-#define int21_seekfile(handle, method, chunklen, len, poschunk, pos, errorflag) asm inline ("movb $66, %%ah\n\tint $33" : "=d" (poschunk), "=a" (pos), "=@ccc" (errorflag) : "b" (handle), "a" (method), "c" (chunklen), "d" (len))
+#define int21_seekfile(handle, method, chunklen, len, poschunk, pos, errorflag) asm volatile ("movb $66, %%ah\n\tint $33" : "=d" (poschunk), "=a" (pos), "=@ccc" (errorflag) : "b" (handle), "a" (method), "c" (chunklen), "d" (len))
 //Supporting defines
 //From the beginning of the file
 #define FILE_SEEK_ABSOLUTE 0x00
